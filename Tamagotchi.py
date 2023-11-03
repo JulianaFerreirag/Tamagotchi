f3549555a035f6af78cae0fe352e0bc260ece52e
Julianaferreirag <jujufreire@gmail.com>
2023-11-03 11:47:32 -0300

    Exercício de pratica de Biblioteca

---
+++ Tamagotchi.py
@@ -0,0 +1,9 @@
+from Biblioteca import Pessoa
+pessoa=Pessoa("Juliana Ferreira", '80', 23)
+pessoa.apresenta()
+pessoa.comer()
+pessoa.pararDeComer()
+pessoa.falar()
+pessoa.pararDeFalar()
+pessoa.dormir()
+pessoa.acordar()
